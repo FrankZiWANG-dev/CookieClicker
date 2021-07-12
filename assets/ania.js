a3545a11a9f05f8e5c477ca2671027d41f28fb7e
AD <anais.dehand@gmail.com>
2021-07-12 16:39:53 +0000

    update

--- assets/ania.js
+++ assets/ania.js
@@ -1,31 +1,18 @@
-var valueClick = 1;
-var priceBtnMulti = 25; // initial price of the button
-const btnMulti = document.getElementById("multiX2");
-const scoreTitle = document.getElementById("score");
+//import { score, NormalClick } from "./marty.js";
 
-// FUNCTION CLICKER
+let score = 0;
+let valueClick = 0;
+let priceBtnMulti = 10; // initial price of the button
+const btnMulti = (document.getElementById("multiX2").disabled = true);
 
-function clicker() {
-  document.getElementById("salty").onclick = function () {
-    var score = parseInt(document.getElementById("score").innerHTML);
-    score++;
-    document.getElementById("score").innerHTML = score;
-  };
-}
-clicker();
+NormalClick();
 
-// DISABLE
+console.log(score);
 
-function disable() {
-  document.getElementById("multiX2").disabled = true;
-  document.getElementById("boost").disabled = true;
-  document.getElementById("auto-click").disabled = true;
-}
-disable();
 if (score >= priceBtnMulti) {
   document.getElementById("multiX2").disabled = false;
 }
-// FUNCTION x2
+//  Click +2
 
 btnMulti.addEventListener("click", () => {
   if (score >= priceBtnMulti) {
--- assets/marty.js
+++ assets/marty.js
@@ -1,11 +1,14 @@
-var score = parseInt(document.getElementById("score").innerHTML);
+// replace score by 0
+
+let score = parseInt(document.getElementById("score").innerHTML);
+
 function NormalClick() {
-    score++;
-    document.getElementById("score").innerHTML = score; 
+  score++;
+  document.getElementById("score").innerHTML = score;
 }
+// replace by addEventListener instead
+document.getElementById("salty").onclick = function () {
+  NormalClick();
+};
 
-document.getElementById('salty').onclick=function(){
-    NormalClick();
-}
-
-export {score, NormalClick};
+export { score, NormalClick };
