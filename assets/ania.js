f6629072070dd878db8624868acbab21fd93aa84
Svanna <svanna@pop-os.localdomain>
2021-07-13 12:26:58 +0200

    update button multi

--- assets/ania.js
+++ assets/ania.js
@@ -1,10 +1,21 @@
 //import { score, NormalClick } from "./marty.js";
 
 let score = 0;
-let priceBtnMulti = 10; // initial price of the button
+let priceBtnMulti = 25; // initial price of the button
 let btnMulti = (document.getElementById("multiX2").disabled = true);
 
 console.log(score);
+
+var Score = 0;
+console.log(Score);
+
+function SingleClick() {
+  if (bonus === 1) {
+    Score += 2;
+  } else if (bonus === 0) {
+    Score++;
+  }
+}
 
 //  Click +2
 
@@ -19,12 +30,16 @@ document.getElementById("salty").addEventListener("click", () => {
 
   check();
 });
-
+var bonus = 0;
 document.getElementById("multiX2").addEventListener("click", () => {
   multi();
+  bonus += 1;
+  Score -= priceBtnMulti;
+  multi(Score);
   document.getElementById("score").innerHTML = Score;
   console.log(score);
   check();
+  document.getElementById("multiX2").disabled = true;
 });
 
 function check() {
@@ -32,4 +47,3 @@ function check() {
     document.getElementById("multiX2").disabled = false;
   }
 }
-check();
