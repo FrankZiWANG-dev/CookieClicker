fecdceadfe38e6defbcda46346cd17ee847a7dcb
martin trufley <pro.trufley@gmail.com>
2021-07-13 16:06:18 +0200

    last modif

--- assets/marty.js
+++ assets/marty.js
@@ -1,12 +1,36 @@
 var Score = parseInt(document.getElementById("score").innerText);
-console.log(Score);
+var hightscrore = 0;
+var archi1 = 0;
+var archi2 = 0;
+var archi3 = 0;
+var archi4 = 0;
 
 function SingleClick() {
         Score++;
+        if (hightscrore <= Score){
+            hightscrore = Score;
+            document.getElementById("score").innerHTML = Score;
+        }
+        if(hightscrore === 10){
+            document.getElementById("achi1").innerHTML = " Your best clicks: " + hightscrore;
+            alert("Good Job !")
+        }
+        if(hightscrore === 100){
+            document.getElementById("achi2").innerHTML = " Your best clicks: " + hightscrore;
+            alert("Great !")
+        }
+        if(hightscrore === 500){
+            document.getElementById("achi3").innerHTML = " Your best clicks: " + hightscrore;
+            alert("Excellent !")
+        }
+        if(hightscrore === 1000){
+            document.getElementById("achi4").innerHTML = " Your best clicks: " + hightscrore;
+            alert("OMFG !")
+        }
     }
 
 document.getElementById('salty').addEventListener("click",() => {
     SingleClick();
-    document.getElementById("score").innerHTML = Score;
-    console.log(Score);
+    document.getElementById("hightscore").innerHTML = hightscrore;
+
 });
